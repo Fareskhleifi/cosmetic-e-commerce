6f2b169cb64d1d246c97a6991bcb1667476c5845
Fares Khleifi <119818420+Fareskhleifi@users.noreply.github.com>
2025-05-09 01:16:25 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
             <Header />
             <main className="flex-grow">
               <Routes>
-                <Route path="/" element={<HomePage />} />
+                <Route path="/cosmetic-e-commerce" element={<HomePage />} />
                 <Route path="/products" element={<ProductsPage />} />
                 <Route path="/products/:id" element={<ProductDetailPage />} />
                 <Route path="/cart" element={<CartPage />} />
